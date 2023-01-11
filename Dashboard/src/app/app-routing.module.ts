c203c2c74493b71ffe5b86b20b54ab6036990ec0
mel-fayne <melaniefayne33@gmail.com>
2023-01-11 06:30:41 +0300

    implement app routing

--- Dashboard/src/app/app-routing.module.ts
+++ Dashboard/src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 import { DashboardComponent } from './core/dashboard/dashboard.component';
 import { ErrorsComponent } from './core/errors/errors.component';
 import { RecentsComponent } from './core/recents/recents.component';
 import { SettingsComponent } from './core/settings/settings.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
-  {path: 'dashboard', component: DashboardComponent },
-  {path: 'errors', component: ErrorsComponent },
-  {path: 'recents', component: RecentsComponent },
-  {path: 'settings', component: SettingsComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  {path: 'errors', component: ErrorsComponent, canActivate: [AuthGuard] },
+  {path: 'recents', component: RecentsComponent, canActivate: [AuthGuard] },
+  {path: 'settings', component: SettingsComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
