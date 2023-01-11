22a640294ed20cb902e041212ef36c0555ac168d
mel-fayne <melaniefayne33@gmail.com>
2023-01-11 06:31:06 +0300

    implement auth controlled layout

--- Dashboard/src/app/app.component.ts
+++ Dashboard/src/app/app.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, map, shareReplay, BehaviorSubject } from 'rxjs';
+import { AuthService } from './auth/auth.service';
 
-interface SideNavToggle{
+interface SideNavToggle {
   screenWidth: number;
   collapsed: boolean;
 }
@@ -8,16 +13,42 @@ interface SideNavToggle{
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Dashboard';
+  title = 'Error Logger';
 
   isSideNavCollapsed = false;
-  screenWidth =0;
-  
+  screenWidth = 0;
+
   onToggleSideNav(data: SideNavToggle): void {
     this.screenWidth = data.screenWidth;
     this.isSideNavCollapsed = data.collapsed
   }
+
+  isLoggedIn$!: Observable<boolean>;
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver, private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+    var loggedIn$ = this.authService.isLoggedIn;
+    if (loggedIn$) {
+      //  get user profile
+      this.isLoggedIn$ = this.authService.isLoggedIn;
+    } else {
+      this.isLoggedIn$ = new BehaviorSubject<boolean>(false);
+      this.router.navigateByUrl('/login');
+    }
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
 }
